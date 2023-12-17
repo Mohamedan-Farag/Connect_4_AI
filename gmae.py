f3a24e0bd2d261bcadec004bed63a87a6ca46165
Mohamedan Farag <120517880+Mohamedan-Farag@users.noreply.github.com>
2023-12-17 20:06:09 +0200

    Add files via upload

---
+++ gmae.py
@@ -0,0 +1,116 @@
+import numpy as np
+import random
+import pygame
+import sys
+import math
+#///////////////////////////////////////////////////////////#
+BLUE = (0,0,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+YELLOW = (255,255,0)
+#///////////////////////////////////////////////////////////#
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+#///////////////////////////////////////////////////////////#
+PLAYER = 0
+AI = 1
+#///////////////////////////////////////////////////////////#
+EMPTY = 0
+PLAYER_PIECE = 1
+AI_PIECE = 2
+WINDOW_LENGTH = 4
+#///////////////////////////////////////////////////////////#
+def create_board():
+	board = np.zeros((ROW_COUNT,COLUMN_COUNT))
+	return board
+
+def drop_piece(board, row, col, piece):
+	board[row][col] = piece
+
+def is_valid_location(board, col):
+	return board[ROW_COUNT-1][col] == 0
+
+def get_next_open_row(board, col):
+	for r in range(ROW_COUNT):
+		if board[r][col] == 0:
+			return r
+
+def print_board(board):
+	print(np.flip(board, 0))
+
+
+def draw_board(board):
+	for c in range(COLUMN_COUNT):
+		for r in range(ROW_COUNT):
+			pygame.draw.rect(screen, BLUE, (c*SQUARESIZE, r*SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
+			pygame.draw.circle(screen, BLACK, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS)
+	
+	for c in range(COLUMN_COUNT):
+		for r in range(ROW_COUNT):		
+			if board[r][c] == PLAYER_PIECE:
+				pygame.draw.circle(screen, RED, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+			elif board[r][c] == AI_PIECE: 
+				pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+	pygame.display.update()
+
+
+#do your job here 
+
+
+
+
+
+
+
+
+
+#///////////////////////////////////////////////////////////#
+board = create_board()
+print_board(board)
+game_over = False
+pygame.init()
+SQUARESIZE = 110
+width = COLUMN_COUNT * SQUARESIZE
+height = (ROW_COUNT+1) * SQUARESIZE
+size = (width, height)
+RADIUS = int(SQUARESIZE/2 - 5)
+screen = pygame.display.set_mode(size)
+draw_board(board)
+pygame.display.update()
+myfont = pygame.font.SysFont("monospace", 75)
+turn = random.randint(PLAYER, AI)
+while not game_over:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
+		if event.type == pygame.MOUSEMOTION:
+			pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+			posx = event.pos[0]
+			if turn == PLAYER:
+				pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
+		pygame.display.update()
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+			# Ask for Player 1 Input
+			if turn == PLAYER:
+				posx = event.pos[0]
+				col = int(math.floor(posx/SQUARESIZE))
+				if is_valid_location(board, col):
+					row = get_next_open_row(board, col)
+					drop_piece(board, row, col, PLAYER_PIECE)
+					turn += 1
+					turn = turn % 2
+					print_board(board)
+					draw_board(board)
+
+	#  Ask for Player 2 Input
+	if turn == AI and not game_over:				
+		col = random.randint(0, COLUMN_COUNT-1)
+		if is_valid_location(board, col):
+			pygame.time.wait(300)
+			row = get_next_open_row(board, col)
+			drop_piece(board, row, col, AI_PIECE)
+			print_board(board)
+			draw_board(board)
+			turn += 1
+			turn = turn % 2
